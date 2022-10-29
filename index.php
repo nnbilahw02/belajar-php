bff15fe46288cecdfa8998a36c05daf97a084e27
Nabila Hermawan <nabilahermawan02@gmail.com>
2022-10-29 20:11:50 +0700

    belajar bahasa php1

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+    echo "Hello, World! <br>";
+
+    $nama = 'Nabila Hermawan';
+    $umur = 20;
+
+    echo "Nama saya <strong> $nama </strong>, dan usia saya sekarang $umur thn. <br>";
+
+    $namaadik = 'Shafira Hermawan';
+    $umuradik = 16;
+
+    echo "Nama adik saya <strong> $namaadik </strong>, dan usia adik saya sekarang $umuradik thn. <br>";
+
+    $selisihumur = $umur % $umuradik;
+    echo "Selisih umur saya dan adik saya yaitu $selisihumur thn. <br>";
+?>
