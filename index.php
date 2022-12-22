f314744b62fc92807e14ebd741c657bf74659bc9
Nabila Hermawan <nabilahermawan02@gmail.com>
2022-12-22 15:42:32 +0700

    membuat crud

--- index.php
+++ index.php
@@ -1,17 +1,68 @@
 <?php
+// Koneksi
+$conn = mysqli_connect("localhost:8080", "root", "", "fakultas");
 
-    echo "Hello, World! <br>";
+// Cek Koneksi
+if (mysqli_connect_errno()) {
+    echo "Gagal Guys " . mysqli_connect_error();
+    exit();
+} else {
+    echo "Koneksi Aman Bestie";
+}
 
-    $nama = 'Nabila Hermawan';
-    $umur = 20;
+// sql query read table
+$sql = "SELECT * FROM mahasiswa";
+$mahasiswa = [];
 
-    echo "Nama saya <strong> $nama </strong>, dan usia saya sekarang $umur thn. <br>";
+if ($result = mysqli_query($conn, $sql)) {
+    while ($row = mysqli_fetch_assoc($result)) {
+        $mahasiswa[] = $row;
+    }
+    mysqli_free_result($result);
+}
 
-    $namaadik = 'Shafira Hermawan';
-    $umuradik = 16;
-
-    echo "Nama adik saya <strong> $namaadik </strong>, dan usia adik saya sekarang $umuradik thn. <br>";
-
-    $selisihumur = $umur % $umuradik;
-    echo "Selisih umur saya dan adik saya yaitu $selisihumur thn. <br>";
+// sql close
+mysqli_close($conn);
 ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Data Mahasiswa</title>
+</head>
+
+<body>
+    <h1 style="text-align: center;">DATA MAHASISWA</h1>
+    <table border="1" style="width: 100%;">
+        <tr>
+            <th>NIM</th>
+            <th>Nama</th>
+            <th>Jenis Kelamin</th>
+            <th>Tempat Lahir</th>
+            <th>Tanggal Lahir</th>
+            <th>Alamat</th>
+            <th>Action</th>
+        </tr>
+        <?php foreach ($mahasiswa as $row) : ?>
+            <tr>
+                <td><?= $row['nim'] ?></td>
+                <td><?= $row['nama'] ?></td>
+                <td><?= $row['jenis_kelamin'] ?></td>
+                <td><?= $row['tempat_lahir'] ?></td>
+                <td><?= $row['tanggal_lahir'] ?></td>
+                <td><?= $row['alamat'] ?></td>
+                <td>
+                    <a href="update.php?id=<?= $row['id'] ?>">Edit</a>
+                    <a href="delete.php?id=<?= $row['id'] ?>">Delete</a>
+                </td>
+            </tr>
+        <?php endforeach; ?>
+    </table>
+</body>
+
+</html>
